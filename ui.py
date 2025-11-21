112fe8bd78e07c526cdf6909a8c4486c6578e54d
Murtaza Hussain <murtaza32004@gmail.com>
2025-11-21 01:22:28 -0800

    testing CICD

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-st.title("📱 Mobile Price Range Predictors")
+st.title("📱 Mobile Price Range Predictor v2")
 
 # -----------------------------
 # Load Model + Feature Order
